4262f3743bcaa9932cb123e7830e96c490fa8b17
Tomasz Suski <tomasz.suski@orba.co>
2024-01-17 15:42:29 +0100

    using loaded data via useLoaderData hook

--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -1,39 +1,15 @@
-import React, { useEffect, useState} from "react";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 import Event from "../models/Event";
 import EventsList from "../components/EventsList";
 
 export interface EventsPageProps {}
 
-export type ErrorType = string | null;
-
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState<Event[]>([]);
-  const [error, setError] = useState<ErrorType>(null);
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        setError('Fetching events failed.');
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
+  const data  = useLoaderData() as Event[];
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={data} />
     </>
   );
 }

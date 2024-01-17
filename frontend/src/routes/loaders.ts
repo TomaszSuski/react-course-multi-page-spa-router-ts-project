f86849d6903bd7aaae6d20c8b8923114e8a45bb8
Tomasz Suski <tomasz.suski@orba.co>
2024-01-17 16:00:52 +0100

    outsourced loader logic

---
+++ frontend/src/routes/loaders.ts
@@ -0,0 +1,10 @@
+export async function eventsLoader() {
+    const response = await fetch("http://localhost:8080/events");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch events.");
+    } else {
+      const resData = await response.json();
+      return resData.events;
+    }
+  }
---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootPage from "../pages/Root";
+import HomePage from "../pages/Home";
+import EventsPage from "../pages/Events";
+import EventsLayoutPage from "../pages/EventsLayout";
+import EventDetailsPage from "../pages/EventDetails";
+import NewEventPage from "../pages/NewEvent";
+import EditEventPage from "../pages/EditEvent";
+import { eventsLoader } from "./loaders";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+
+      {
+        path: "events",
+        element: <EventsLayoutPage />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          { path: ":id", element: <EventDetailsPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":id/edit", element: <EditEventPage /> },
+        ],
+      },
+    ],
+  },
+]);

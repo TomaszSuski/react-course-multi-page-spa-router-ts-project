fe994a33f1ab0893e314740b0958ad04ccbd85d6
Tomasz Suski <tomasz.suski@orba.co>
2024-01-23 12:32:40 +0100

    added navigation to error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PageContent from "../components/PageContent";
 import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 
 export interface ErrorPageProps {}
 
@@ -31,8 +32,11 @@ export default function ErrorPage(props: ErrorPageProps) {
   }
 
   return (
-    <PageContent title={title}>
-      <p>{message}</p>
-    </PageContent>
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
   );
 }

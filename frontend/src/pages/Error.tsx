6e5db219a2b53122dde9aa8846cc12f31ab786e0
Tomasz Suski <tomasz.suski@orba.co>
2024-01-23 12:27:27 +0100

    throwing new response for error and added proper error handling for 404 and culd not fetch errors

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
 
 export interface ErrorPageProps {}
 
+type ErrorWithMessage = {
+  status: number;
+  data: { message: string };
+};
+type ErrorWithText = {
+  status: number;
+  data: string;
+};
+
 export default function ErrorPage(props: ErrorPageProps) {
+  const error = useRouteError() as ErrorWithMessage | ErrorWithText;
+
+  let title = "An error occured";
+  let message = "Something went wrong";
+
+  if (error.status === 404) {
+    title = "Page not found";
+    message = "The page you are looking for does not exist";
+  }
+
+  if (error.status === 500) {
+    if (typeof error.data === "string") {
+      message = JSON.parse(error.data).message;
+    }
+  }
+
   return (
-    <PageContent title="An error occured">
-      <p>Something went wrong</p>
+    <PageContent title={title}>
+      <p>{message}</p>
     </PageContent>
   );
 }
--- frontend/src/routes/loaders.ts
+++ frontend/src/routes/loaders.ts
@@ -1,12 +1,15 @@
 export async function eventsLoader() {
-    const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/events");
 
-    if (!response.ok) {
+  if (!response.ok) {
     //   return {isError: true, message: "Could not load events"};
-    throw new Error("Could not load events");
-    } else {
+    // throw new Error("Could not load events");
+    throw new Response(JSON.stringify({ message: "Could not load events" }), {
+      status: 500,
+    });
+  } else {
     //   const resData = await response.json();
     //   return resData.events;
     return response;
-    }
   }
+}
